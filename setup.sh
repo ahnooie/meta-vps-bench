533ece38ff34086d5d29378de5651924f6223e4d
Benjamin Bryan <ben@b3n.org>
2016-12-11 21:11:49 -0800

    sysbench with json output

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# install mariadb, sysbench
+# Install mariadb, sysbench, speedtest_cli, python
+
 export DEBIAN_FRONTEND=noninteractive
 debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password test'
 debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password test'
@@ -8,8 +9,8 @@ debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again
 apt-get update
 apt-get -y upgrade
 apt-get -y install mariadb-server
-apt-get -y install sysbench
 apt-get -y install python3 python3-pip
+apt-get -y install libmysqlclient-dev
 
 pip3 install speedtest-cli
 
@@ -18,4 +19,16 @@ sysbench --test=fileio --file-total-size=6G prepare
 
 mysql -uroot --password=test -e "create database test"
 sysbench --test=oltp --oltp-table-size=1000000 --mysql-db=test --mysql-user=root --mysql-password=test prepare
+
+# Compile sysbench with json-output support
+
+git clone https://github.com/ahnooie/meta-vps-bench.git
+cd sysbench
+git checkout 1.0-report-styles
+git pull
+./autogen.sh
+./configure
+make
+make install
+
 
