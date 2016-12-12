30133c4252d44b677900a65f88ca35553812ecb3
Benjamin Bryan <ben@b3n.org>
2016-12-11 17:24:41 -0800

    variety of speedtest locations

--- bench.sh
+++ bench.sh
@@ -49,5 +49,92 @@ sysbench --test=memory --max-time=300 --memory-total-size=1000M run
 echo "**** MEMORY 2000M" 
 sysbench --test=memory --max-time=300 --memory-total-size=2000M run 
 
+echo "**** SPEEDTEST DEFAULT"
+speedtest-cli
+
+# SpeedTest Locations
+
+# Argentina
+#  900) Telecentro (Buenos Aires, Argentina) [11156.20 km]
+speedtest-cli --server 900 
+
+# Austria
+# 1738) www.drei.at (Vienna, Austria) [8571.38 km]
+speedtest-cli --server 1738
+
+# Brazil
+#  3068) TIM Brasil (Sao Paulo, Brazil) [10920.56 km]
+speedtest-cli --server 3068 
+
+# China 
+# 3633) China Telecom (Shanghai, China) [9167.07 km]
+speedtest-cli --server 3633 
+
+# France
+# 9192) Silverblades.fr (Paris, France) [7972.84 km]
+speedtest-cli --server 9192 
+
+# Germany
+# 1475) IPB (Berlin, Germany) [8049.48 km]
+speedtest-cli --server 1475 
+# 2398) Studio Funk GmbH & Co. KG (Hamburg, Germany) [7833.78 km]
+speedtest-cli --server 2398 
+
+# Hong Kong
+# 1536) STC (Hong Kong, China) [10397.15 km]
+speedtest-cli --server 1536 
+
+# India
+# 1718) Vodafone India (Mumbai, India) [12404.46 km]
+speedtest-cli --server 1718 
+
+# Japan
+# 7510) ASEINet (Tokyo, Japan) [7686.34 km]
+speedtest-cli --server 7510
+
+# Russia
+# 3170) Comstar-r (Nizhny Novgorod, Russian Federation) [8322.08 km]
+speedtest-cli --server 3170 
+
+# Spain
+# 3863) Orange (Madrid, Spain) [8449.11 km]
+speedtest-cli --server 3863 
+
+# South Korea
+# 6527) kdatacenter.com (Seoul, South Korea) [8297.88 km]
+speedtest-cli --server 6527 
+
+# United Kingdom
+
+# 2789) Vodafone UK (London, Great Britain) [7632.74 km]
+speedtest-cli --server 2789 
+# 1531) Cloud Space UK (Canterbury, Great Britain) [7704.11 km]
+speedtest-cli --server 1531 
+
+
+
+# United States
+
+# 3165) Georgia Institute of Technology (Atlanta, GA, United States) [3494.75 km]
+speedtest-cli --server 3165
+
+# 10386) Speedtest.net (Dallas, TX, United States) [2711.58 km]
+speedtest-cli --server 10386
+
+# 10392) Speedtest.net (Los Angeles, CA, United States) [1601.78 km]
+speedtest-cli --server 10392
+
+# 2947) Atlantic Metro (New York City, NY, United States) [3833.63 km]
+speedtest-cli --server 2947
+
+# 7519) Quonix, Inc. (Philadelphia, PA, United States) [3789.44 km]
+speedtest-cli --server 7519
+
+#  603) Unwired (San Francisco, CA, United States) [1157.58 km]
+speedtest-cli --server 603
+
+# 1776) Comcast (Chicago, IL, United States) [2762.90 km]
+speedtest-cli --server 1776  
+
 
 } >> result
