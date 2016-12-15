18c179f064dab91236a9131cbc66579af848e9d1
Benjamin Bryan <ben@b3n.org>
2016-12-14 23:38:58 -0800

    parsing speedtest

--- bench.sh
+++ bench.sh
@@ -47,85 +47,85 @@ speedtest-cli
 
 # Argentina
 #  900) Telecentro (Buenos Aires, Argentina) [11156.20 km]
-speedtest-cli --server 900  >> speedtest900.json
+speedtest-cli --server 900  >> speedtest900.result
 
 # Austria
 # 1738) www.drei.at (Vienna, Austria) [8571.38 km]
-speedtest-cli --server 1738  >> speedtest1738.json
+speedtest-cli --server 1738  >> speedtest1738.result
 
 # Brazil
 #  3068) TIM Brasil (Sao Paulo, Brazil) [10920.56 km]
-speedtest-cli --server 3068  >> speedtest3068.json 
+speedtest-cli --server 3068  >> speedtest3068.result 
 
 # China 
 # 3633) China Telecom (Shanghai, China) [9167.07 km]
-speedtest-cli --server 3633  >> speedtest3633.json 
+speedtest-cli --server 3633  >> speedtest3633.result 
 
 # France
 # 9192) Silverblades.fr (Paris, France) [7972.84 km]
-speedtest-cli --server 9192  >> speedtest9192.json 
+speedtest-cli --server 9192  >> speedtest9192.result 
 
 # Germany
 # 1475) IPB (Berlin, Germany) [8049.48 km]
-speedtest-cli --server 1475  >> speedtest1475.json 
+speedtest-cli --server 1475  >> speedtest1475.result 
 # 2398) Studio Funk GmbH & Co. KG (Hamburg, Germany) [7833.78 km]
-speedtest-cli --server 2398  >> speedtest2398.json 
+speedtest-cli --server 2398  >> speedtest2398.result 
 
 # Hong Kong
 # 1536) STC (Hong Kong, China) [10397.15 km]
-speedtest-cli --server 1536  >> speedtest1536.json 
+speedtest-cli --server 1536  >> speedtest1536.result 
 
 # India
 # 1718) Vodafone India (Mumbai, India) [12404.46 km]
-speedtest-cli --server 1718  >> speedtest1718.json 
+speedtest-cli --server 1718  >> speedtest1718.result 
 
 # Japan
 # 7510) ASEINet (Tokyo, Japan) [7686.34 km]
-speedtest-cli --server 7510  >> speedtest7510.json
+speedtest-cli --server 7510  >> speedtest7510.result
 
 # Russia
 # 3170) Comstar-r (Nizhny Novgorod, Russian Federation) [8322.08 km]
-speedtest-cli --server 3170  >> speedtest3170.json 
+speedtest-cli --server 3170  >> speedtest3170.result 
 
 # Spain
 # 3863) Orange (Madrid, Spain) [8449.11 km]
-speedtest-cli --server 3863  >> speedtest3863.json 
+speedtest-cli --server 3863  >> speedtest3863.result 
 
 # South Korea
 # 6527) kdatacenter.com (Seoul, South Korea) [8297.88 km]
-speedtest-cli --server 6527  >> speedtest6527.json 
+speedtest-cli --server 6527  >> speedtest6527.result 
 
 # United Kingdom
 
 # 2789) Vodafone UK (London, Great Britain) [7632.74 km]
-speedtest-cli --server 2789  >> speedtest2789.json 
+speedtest-cli --server 2789  >> speedtest2789.result 
 # 1531) Cloud Space UK (Canterbury, Great Britain) [7704.11 km]
-speedtest-cli --server 1531  >> speedtest1531.json 
+speedtest-cli --server 1531  >> speedtest1531.result 
 
 
 
 # United States
 
 # 3165) Georgia Institute of Technology (Atlanta, GA, United States) [3494.75 km]
-speedtest-cli --server 3165  >> speedtest3165.json
+speedtest-cli --server 3165  >> speedtest3165.result
 
 # 10386) Speedtest.net (Dallas, TX, United States) [2711.58 km]
-speedtest-cli --server 10386  >> speedtest10386.json
+speedtest-cli --server 10386  >> speedtest10386.result
 
 # 10392) Speedtest.net (Los Angeles, CA, United States) [1601.78 km]
-speedtest-cli --server 10392  >> speedtest10392.json
+speedtest-cli --server 10392  >> speedtest10392.result
 
 # 2947) Atlantic Metro (New York City, NY, United States) [3833.63 km]
-speedtest-cli --server 2947  >> speedtest2947.json
+speedtest-cli --server 2947  >> speedtest2947.result
 
 # 7519) Quonix, Inc. (Philadelphia, PA, United States) [3789.44 km]
-speedtest-cli --server 7519  >> speedtest7519.json
+speedtest-cli --server 7519  >> speedtest7519.result
 
 #  603) Unwired (San Francisco, CA, United States) [1157.58 km]
-speedtest-cli --server 603  >> speedtest603.json
+speedtest-cli --server 603  >> speedtest603.result
 
 # 1776) Comcast (Chicago, IL, United States) [2762.90 km]
-speedtest-cli --server 1776  >> speedtest1776.json  
+speedtest-cli --server 1776  >> speedtest1776.result  
 
 
 } >> result.log
