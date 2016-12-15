94c238ddabc8820fb9aa7c55b2ca003cec8f93c0
Benjamin Bryan <ben@b3n.org>
2016-12-14 22:55:02 -0800

    back to sysbench from repo

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ apt-get -y upgrade
 apt-get -y install mariadb-server
 apt-get -y install python3 python3-pip
 apt-get -y install libmysqlclient-dev
+apt-get -y install sysbench
 
 pip3 install speedtest-cli
 
@@ -20,15 +21,5 @@ sysbench --test=fileio --file-total-size=6G prepare
 mysql -uroot --password=test -e "create database test"
 sysbench --test=oltp --oltp-table-size=1000000 --mysql-db=test --mysql-user=root --mysql-password=test prepare
 
-# Compile sysbench with json-output support
-
-git clone https://github.com/ahnooie/sysbench.git
-cd sysbench
-git checkout 1.0-report-styles
-git pull
-./autogen.sh
-./configure
-make
-make install
 
 
