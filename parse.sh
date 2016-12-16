7803000804d3d897fcaec5929f6de38d02a40ddd
Benjamin Bryan <ben@b3n.org>
2016-12-15 19:21:07 -0800

    rm more

--- parse.sh
+++ parse.sh
@@ -6,9 +6,9 @@ RESULT_CPU1="$(cat cpu1.result |grep "total time:"| cut -f2 -d":"|xargs)"
 RESULT_CPU2="$(cat cpu2.result |grep "total time:"| cut -f2 -d":"|xargs)"
 RESULT_CPU4="$(cat cpu4.result |grep "total time:"| cut -f2 -d":"|xargs)"
 
-RESULT_OLTP1="$(cat more oltp1.result |grep transactions: |cut -f2 -d"("|cut -f1 -d")"|xargs)"
-RESULT_OLTP4="$(cat more oltp4.result |grep transactions: |cut -f2 -d"("|cut -f1 -d")"|xargs)"
-RESULT_OLTP8="$(cat more oltp8.result |grep transactions: |cut -f2 -d"("|cut -f1 -d")"|xargs)"
+RESULT_OLTP1="$(cat oltp1.result |grep transactions: |cut -f2 -d"("|cut -f1 -d")"|xargs)"
+RESULT_OLTP4="$(cat oltp4.result |grep transactions: |cut -f2 -d"("|cut -f1 -d")"|xargs)"
+RESULT_OLTP8="$(cat oltp8.result |grep transactions: |cut -f2 -d"("|cut -f1 -d")"|xargs)"
 
 RESULT_RNDWR="$(cat rmdwr.result|grep Requests|cut -f1 -d\"R\"|xargs)"
 RESULT_RNDRD="$(cat rmdrd.result|grep Requests|cut -f1 -d\"R\"|xargs)"
