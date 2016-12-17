878004135897c9ae4d8af87e9ad20509bd34fe2a
Benjamin Bryan <ben@b3n.org>
2016-12-17 13:23:40 -0800

    Use go speedtest (#4)
    use go speedtest

--- setup.sh
+++ setup.sh
@@ -9,11 +9,13 @@ debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again
 apt-get update
 apt-get -y upgrade
 apt-get -y install mariadb-server
-apt-get -y install python3 python3-pip
+# apt-get -y install python3 python3-pip
 apt-get -y install libmysqlclient-dev
 apt-get -y install sysbench
+wget https://github.com/zpeters/speedtest/releases/download/speed-issues/speedtest-linux-amd64-speed-issues
+chmod 755 speedtest-linux-amd64-speed-issues
 
-pip3 install speedtest-cli
+#pip3 install speedtest-cli
 
 
 sysbench --test=fileio --file-total-size=6G prepare
